bd3d4ffab69c7cdf16f88bd4ac3d4a719be8e5ff
ahmetemregurdal <ahmetemregurdal2008@gmail.com>
2023-05-26 19:45:57 +0300

    added the files properly

--- neovim/init.lua
+++ neovim/init.lua
@@ -1,1 +1,111 @@
-.config/nvim/init.lua
+vim.opt.bg = "dark"
+vim.opt.compatible = false
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.hidden = true
+vim.opt.fileformat = "unix"
+vim.opt.ignorecase = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.termguicolors = true
+vim.opt.wildmenu = true
+vim.opt.wildmode = "longest:full,full"
+vim.opt.wildoptions = "pum"
+vim.opt.encoding = "UTF-8"
+vim.opt.mouse = "a"
+vim.opt.confirm = true
+vim.opt.history = 1000
+vim.opt.wrap = false
+vim.opt.smartindent = true
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+      "git",
+      "clone",
+      "--filter=blob:none",
+      "https://github.com/folke/lazy.nvim.git",
+      "--branch=stable",
+      lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+vim.g.mapleader = " "
+require("lazy").setup({
+    "searleser97/cpbooster.vim",
+    "morhetz/gruvbox",
+    "mbbill/undotree",
+    "psliwka/vim-smoothie",
+    "ycm-core/YouCompleteMe",
+    "christoomey/vim-system-copy",
+    "jszakmeister/vim-togglecursor",
+    "fladson/vim-kitty",
+    "mboughaba/i3config.vim",
+    {"nvim-treesitter/nvim-treesitter", build = "TSUpdate"},
+    {"nvim-neo-tree/neo-tree.nvim", branch = "v2.x", dependencies = {"nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", "MunifTanjim/nui.nvim" }},
+		{"nvim-lualine/lualine.nvim", dependencies = {"nvim-tree/nvim-web-devicons"}},
+		"lervag/vimtex"
+  })
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'gruvbox',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = true,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
+}
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = { "c", "lua", "yuck", "cpp", "bash", "vim", "vimdoc", "query", "python"},
+  sync_install = true,
+  auto_install = true,
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = {"bash"},
+  },
+}
+vim.cmd([[
+  filetype on
+  syntax enable
+  filetype plugin indent on
+  colorscheme gruvbox
+	let g:neo_tree_remove_legacy_commands = 1
+  :NeoTreeShow
+	let g:vimtex_view_general_viewer = 'okular'
+	let g:vimtex_view_general_options = '--unique file:@pdf\#src:@line@tex'
+	let g:vimtex_compiler_method = 'latexrun'
+	autocmd bufenter * if (winnr("$") == 1 && &filetype == "neo-tree") | q | endif
+	nmap <C-A> :Addtc<CR>
+	nmap <C-T> :Test<CR>
+]])
