9f30b33e50ac0c44a061e8842c8d1bb1466d0662
ahmetemregurdal <ahmetemregurdal2008@gmail.com>
2023-06-03 21:13:42 +0300

    a bunch of stuff changed

--- neovim/init.lua
+++ neovim/init.lua
@@ -5,9 +5,9 @@ vim.opt.relativenumber = true
 vim.opt.hidden = true
 vim.opt.fileformat = "unix"
 vim.opt.ignorecase = true
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.termguicolors = true
 vim.opt.wildmenu = true
 vim.opt.wildmode = "longest:full,full"
@@ -34,20 +34,22 @@ require("lazy").setup({
     "searleser97/cpbooster.vim",
     "morhetz/gruvbox",
     "mbbill/undotree",
-		{"SirVer/ultisnips",dependencies = {"honza/vim-snippets"}},
+	{"SirVer/ultisnips",dependencies = {"honza/vim-snippets"}},
     "psliwka/vim-smoothie",
-		"ycm-core/YouCompleteMe",
+	"ycm-core/YouCompleteMe",
     "christoomey/vim-system-copy",
     "jszakmeister/vim-togglecursor",
     "fladson/vim-kitty",
     "mboughaba/i3config.vim",
     {"nvim-treesitter/nvim-treesitter", build = "TSUpdate"},
     {"nvim-neo-tree/neo-tree.nvim", branch = "v2.x", dependencies = {"nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", "MunifTanjim/nui.nvim" }},
-		{"nvim-lualine/lualine.nvim", dependencies = {"nvim-tree/nvim-web-devicons"}},
-		"lervag/vimtex",
-		{"goolord/alpha-nvim",dependencies = {"nvim-tree/nvim-web-devicons","nvim-telescope/telescope.nvim","nvim-lua/plenary.nvim"}},
-		"phaazon/hop.nvim",
-		"folke/which-key.nvim"
+	{"nvim-lualine/lualine.nvim", dependencies = {"nvim-tree/nvim-web-devicons"}},
+	"lervag/vimtex",
+	"phaazon/hop.nvim",
+	"folke/which-key.nvim",
+	{'akinsho/toggleterm.nvim', version = "*", config = true},
+	{'romgrk/barbar.nvim', dependencies = { 'lewis6991/gitsigns.nvim', 'nvim-tree/nvim-web-devicons',},init = function() vim.g.barbar_auto_setup = false end,
+	opts = {animation = true,},},
   })
 require('lualine').setup {
   options = {
@@ -89,21 +91,20 @@ require('lualine').setup {
   inactive_winbar = {},
   extensions = {}
 }
-require'alpha'.setup(require'alpha.themes.dashboard'.config)
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "c", "lua", "yuck", "cpp", "bash", "vim", "vimdoc", "query", "python"},
-  sync_install = true,
-  auto_install = true,
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = {"bash"},
+	ensure_installed = { "c", "lua", "yuck", "cpp", "bash", "vim", "vimdoc", "query", "python"},
+	sync_install = true,
+	auto_install = true,
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = {"bash"},
   },
 }
 vim.cmd([[
-  filetype on
-  syntax enable
-  filetype plugin indent on
-  colorscheme gruvbox
+	filetype on
+	syntax enable
+	filetype plugin indent on
+	colorscheme gruvbox
 	let g:neo_tree_remove_legacy_commands = 1
 	let g:vimtex_view_general_viewer = 'okular'
 	let g:vimtex_view_general_options = '--unique file:@pdf\#src:@line@tex'
@@ -111,48 +112,8 @@ vim.cmd([[
 	autocmd bufenter * if (winnr("$") == 1 && &filetype == "neo-tree") | q | endif
 	nmap <C-A> :Addtc<CR>
 	nmap <C-T> :Test<CR>
-	nmap <c-S> :Submit<CR>
-	let g:coc_global_extensions = ['coc-json', 'coc-git', 'coc-clangd' ,'coc-sh']
+	nmap <C-S> :Submit<CR>
+	nmap tt :ToggleTerm direction=float<CR>
+	:NeoTreeShow
+	nmap ll :Lazy<CR>
 ]])
-local status_ok, alpha = pcall(require, "alpha")
-if not status_ok then
- return
-end
-
-local dashboard = require("alpha.themes.dashboard")
-dashboard.section.header.val = {
-
-  [[                                                                       ]],
-	[[                                                                     ]],
-	[[       ████ ██████           █████      ██                     ]],
-	[[      ███████████             █████                             ]],
-	[[      █████████ ███████████████████ ███   ███████████   ]],
-	[[     █████████  ███    █████████████ █████ ██████████████   ]],
-	[[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
-	[[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
-	[[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
-	[[                                                                       ]],
-
-}
-
- dashboard.section.buttons.val = {
-   dashboard.button("f", "Find file", ":Telescope find_files <CR>"),
-   dashboard.button("e", "New file", ":ene <BAR> <CR>:NeoTreeShow<CR>i"),
-   dashboard.button("r", "Recently used files", ":Telescope oldfiles <CR>"),
-   dashboard.button("t", "Find text", ":Telescope live_grep <CR>"),
-   dashboard.button("c", "Configuration", ":e ~/.config/nvim/init.lua<CR>:NeoTreeShow<CR>"),
-   dashboard.button("q", "Quit Neovim", ":qa<CR>"),
-}
-
-local function footer()
- return "Don't Stop Until You are Proud..."
-end
-
-dashboard.section.footer.val = footer()
-
-dashboard.section.footer.opts.hl = "Type"
-dashboard.section.header.opts.hl = "Include"
-dashboard.section.buttons.opts.hl = "Keyword"
-
-dashboard.opts.opts.noautocmd = true
-alpha.setup(dashboard.opts)
